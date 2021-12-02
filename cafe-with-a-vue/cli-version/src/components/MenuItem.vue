36347b96a25c0cc7b1c561325b2003a829e23f98
gylgyl2000 <gylgyl2000@gmail.com>
2021-12-02 20:10:10 +0100

    Entraînement

---
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'MenuItem',
+    props: ['url', 'text']
+}
+</script>
+
+<template>
+    <img
+        class="menu-item__image"
+        :src="item.image.source"
+        :alt="item.image.alt"
+    />
+</template>
