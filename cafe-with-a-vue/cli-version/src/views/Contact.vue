0dfcc337816c635d3e6c5d29ba8c2dfad4a7ca71
gylgyl2000 <gylgyl2000@gmail.com>
2021-12-04 16:44:05 +0100

    Entraînement

---
+++ cafe-with-a-vue/cli-version/src/views/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="contact">
+    <h2>Contactez nous</h2>
+		<p>Adresse : {{ address }}</p>
+		<p>Téléphone : {{ phone }}</p>
+		<p>Email : {{ email }}</p>
+		<p>Horaires :</p>
+		<ul>
+			<li>L-V: 06:00 à 16:00</li>
+			<li>Samedi: 07:00 à 14:00</li>
+			<li>Dimanche: 07:00 à 12:00</li>
+		</ul>
+  </div>
+</template>
